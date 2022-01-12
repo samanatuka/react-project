f200d8c66cbc3b97c47894c6db3d2972771eb969
samanatuka <60912766+samanatuka@users.noreply.github.com>
2022-01-12 09:33:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div>
+      <button onClick={()=>{
+        console.log("clicked")
+      }}>Submit</button>
+      
+    </div>
+  );
+}
+
+export default App;
